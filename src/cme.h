e61000add96946d0646e8145c3664089007e968f
Dave Favis-Mortlock <dfm1@bgs.ac.uk>
2025-03-24 14:40:23 +0000

    1.3.6

--- src/cme.h
+++ src/cme.h
@@ -709,7 +709,7 @@ double const CLIFF_COLLAPSE_HEIGHT_INCREMENT = 0.1;      // Increment the fracti
 
 double const DBL_NODATA = -9999;
 
-string const PROGRAM_NAME = "Coastal Modelling Environment (CoastalME) version 1.3.5 (28 Mar 2025)";
+string const PROGRAM_NAME = "Coastal Modelling Environment (CoastalME) version 1.3.6 (28 Mar 2025)";
 string const PROGRAM_NAME_SHORT = "CME";
 string const CME_INI = "cme.ini";
 
